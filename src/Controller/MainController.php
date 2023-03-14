339d528553dd536496f6ad358c975abfcae4c315
lcaffarelli2022 <laetitia.caffarelli@gmail.com>
2023-03-14 17:23:47 +0100

    ajout dans la nav barre ajout du wishcontroller ajout de la css ajout du about us

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,12 +3,15 @@
 namespace App\Controller;
 use \Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
+#[Route("/", name: "main_")]
 class MainController extends AbstractController
 {
-    #[Route("/", name: "home")]
+    #[Route("", name: "home")]
 public function home(){
     return $this->render("home.html.twig");
 }
-
+#[Route("/aboutUs", name: "aboutUs")]
+ public function infos(){
+        return$this->render("aboutUs.html.twig");
+ }
 }
--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -5,14 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-#[Route("/", name: "whish_")]
+#[Route("/wish", name: "wish_")]
 class WishController extends AbstractController
 {
     #[Route('', name: 'list')]
-    public function addWish(): Response
+    public function list(): Response
     {
         return $this->render('/wish/list.html.twig', [
 
         ]);
+    }
+#[Route("/details/{id}")]
+    public function details(int $id){
+        return $this->render("/wish/details.html.twig");
     }
 }
