d14dea8d0a8095621b06609855142e2c400396ac
lcaffarelli2022 <laetitia.caffarelli@gmail.com>
2023-03-15 14:19:10 +0100

    ajout des fichiers non versionnés

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,17 +1,23 @@
 <?php
 
 namespace App\Controller;
+
 use \Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+
 #[Route("/", name: "main_")]
 class MainController extends AbstractController
 {
     #[Route("", name: "home")]
-public function home(){
-    return $this->render("home.html.twig");
-}
-#[Route("/aboutUs", name: "aboutUs")]
- public function infos(){
-        return$this->render("aboutUs.html.twig");
- }
+    public function home()
+    {
+        return $this->render("main/home.html.twig");
+    }
+
+    #[Route("/aboutUs", name: "aboutUs")]
+    public function infos()
+    {
+ 
+        return $this->render("main/aboutUs.html.twig");
+    }
 }
--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -5,17 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-#[Route("/wish", name: "wish_")]
+#[Route("/wish/", name: "wish_")]
 class WishController extends AbstractController
 {
-    #[Route('', name: 'list')]
+    #[Route('list', name: 'list')]
     public function list(): Response
     {
-        return $this->render('/wish/list.html.twig', [
+        return $this->render('wish/list.html.twig', [
 
         ]);
     }
-#[Route("/details/{id}")]
+#[Route("details/{id}", name: "details")]
     public function details(int $id){
         return $this->render("/wish/details.html.twig");
     }
